affa834e02247d2096867fd0f4e5f9d071856984
Shai Alon <shaialon84@gmail.com>
2016-03-08 10:52:42 +0200

    styling fixes

--- src/app/components/feed_showcase.js
+++ src/app/components/feed_showcase.js
@@ -18,16 +18,19 @@ var imageHeightRatio = isTablet ? config.imgSizeHeightToWidthRatio.tablet : conf
 var imageHeight = Math.floor(imageHeightRatio * imageWidth);
 
 
-
-
-
 module.exports = function( feedConfig , tab) {
 	var style = cellStyle(feedConfig);
 
 	var container = tabris.create("Composite", { left: 0, right: 0, top: "prev()", height: 200}).appendTo(tab);
-	var header = tabris.create("Composite", style.header ).appendTo(container);
 
+	var header = tabris.create("Composite", style.header ).appendTo(container);
 	tabris.create('TextView',  style.headerText).appendTo(header);
+	tabris.create('TextView',  style.headerSeeAll).appendTo(header);
+	header.on('tap',function(){
+		console.log("See all");
+	});
+
+
 	var itemShowcase = tabris.create("ScrollView", style.itemShowcase).appendTo(container);
 	tabris.create("Composite", style.itemShowcaseScrollHider).appendTo(container);
 
@@ -42,7 +45,8 @@ function cellStyle(feedConfig){
 		itemShowcaseScrollHider: { layoutData: {left: 0,  height:8,  bottom: 0, right: 0},  background: "white"},
 		itemShowcase: { layoutData: {left: 0,  top:42,  bottom: 0, right: 0}, direction: "horizontal", _feed: feedConfig},
 		header: {left: 0, right: 0, height: 30, top:0, background: themeStyle.showcase.background},
-		headerText: { maxLines: 1, font: 'bold 16px', left: 10, right: 0, bottom: 5, top:5, text:feedConfig.name, textColor: themeStyle.showcase.textColor , alignment:'left' }
+		headerText: { maxLines: 1, font: 'bold 16px', left: 10, right: 0, bottom: 5, top:5, text:feedConfig.name, textColor: themeStyle.showcase.textColor , alignment:'left' },
+		headerSeeAll: { maxLines: 1, font: '12px', width: 100, right: 10, bottom: 5, top:5, text: "See all >", textColor: themeStyle.showcase.textColor , alignment:'right' }
 	}
 }
 
@@ -52,35 +56,17 @@ function refreshItems( widget ) {
 	var feedConfig = widget.get('_feed');
 
 
-
 	getItems( feedConfig ).then( function(results){
 		var arr = [].concat(results.items);
 
 		arr.forEach(function(feedItem, index){
 			if(index< 15){
-				var imageUrl = resizeImageURLByWidth(feedItem.image, 120);
-				if(imageUrl){
-					var comp = tabris.create('Composite', { left: ["prev()", 10], width: 90, top: 0, bottom: 0}).appendTo(widget);
-					tabris.create('ImageView', { image:imageUrl,left: 0, right: 0, top: 0, height: 120, scaleMode: 'fill' , background: "rgb(220, 220, 220)"}).appendTo(comp);
-					if(feedItem.price){
-						tabris.create('TextView', { text: '$'+Math.round(feedItem.price),left: 0, right: 0, bottom: 3, height: 30, textColor: "#aaa",  alignment:'center'}).appendTo(comp);
-					}
-
-					comp.on('tap',function(){
-						detailScreen.open(feedItem.title, feedItem);
-					});
-				}
-
+				appendItemBox(widget,feedItem);
 			}
-
 		});
 
-
 		if(results.state && results.state.count){
-			var comp = tabris.create('Composite', { left: ["prev()", 10], width: 160, top: 0, bottom: 0 }).appendTo(widget);
-			var comp2 = tabris.create('Composite', { left: 20, right: 20, top: 20, bottom: 30, background: feedConfig.color}).appendTo(comp);
-			tabris.create('TextView', { text: ''+ '' + "See more<br/>'" +feedConfig.name +"'"  , maxLines: 2, font: '16px', left: 0, right: 20, bottom: 0, top: 0, textColor: "white",  alignment:'center', markupEnabled:true}).appendTo(comp2);
-			tabris.create('TextView', { text: '>'  , maxLines: 1, font: '20px', width: 14, right: 6, bottom: 0, top: 0, textColor: "white",  alignment:'center'}).appendTo(comp2);
+			appendSeeAllBox(widget);
 		}
 
 	}).catch(function(err){
@@ -98,6 +84,34 @@ function updateWidgetLoading(widget,loading){
 	widget.set({
 		refreshIndicator: loading,
 		refreshMessage: loading ? "loading feed..." : ""
+	});
+}
+
+function appendItemBox(widget , feedItem){
+	var imageUrl = resizeImageURLByWidth(feedItem.image, 120);
+	if(imageUrl){
+		var boxContainer = tabris.create('Composite', { left: ["prev()", 10], width: 90, top: 0, bottom: 0}).appendTo(widget);
+		tabris.create('ImageView', { image:imageUrl,left: 0, right: 0, top: 0, height: 120, scaleMode: 'fill' , background: "rgb(220, 220, 220)"}).appendTo(boxContainer);
+		if(feedItem.price){
+			tabris.create('TextView', { text: '$'+Math.round(feedItem.price),left: 0, right: 0, bottom: 3, height: 30, textColor: "#aaa",  alignment:'center'}).appendTo(boxContainer);
+		}
+		boxContainer.on('tap',function(){
+			detailScreen.open(feedItem.title, feedItem);
+		});
+	}
+}
+
+
+function appendSeeAllBox(widget){
+	var feedConfig = widget.get('_feed');
+
+	var boxContainer = tabris.create('Composite', { left: ["prev()", 10], width: 160, top: 0, bottom: 0 }).appendTo(widget);
+	var seeAllBox = tabris.create('Composite', { left: 20, right: 20, top: 20, bottom: 30, background: feedConfig.color}).appendTo(boxContainer);
+	tabris.create('TextView', { text: ''+ '' + "See all<br/>'" +feedConfig.name +"'"  , maxLines: 2, font: '16px', left: 0, right: 20, bottom: 0, top: 0, textColor: "white",  alignment:'center', markupEnabled:true}).appendTo(seeAllBox);
+	tabris.create('TextView', { text: '>'  , maxLines: 1, font: '20px', width: 14, right: 6, bottom: 0, top: 0, textColor: "white",  alignment:'center'}).appendTo(seeAllBox);
+
+	seeAllBox.on('tap',function(){
+		console.log("See all");
 	});
 }
 
