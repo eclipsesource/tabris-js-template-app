c360d181b782a9c3be87017d14aa94dbe3b10918
Shai Alon <shaialon84@gmail.com>
2016-03-20 20:21:13 +0200

    Remove tablet logic

--- src/app/components/item_list.js
+++ src/app/components/item_list.js
@@ -15,7 +15,7 @@ var tabletColumnRight = ''+ (100-tabletWidthRatio*100) +'%';
 
 
 var isTablet = sizing.isTablet;
-var imageWidth = isTablet ? tabris.device.get("screenWidth") * tabletWidthRatio : tabris.device.get("screenWidth");
+var imageWidth = tabris.device.get("screenWidth");
 var imageHeightRatio = isTablet ? config.imgSizeHeightToWidthRatio.tablet : config.imgSizeHeightToWidthRatio.phone;
 var imageHeight = Math.floor(imageHeightRatio * imageWidth);
 
@@ -25,16 +25,10 @@ var CELLS_PER_ROW = 2;
 
 module.exports = function( feedConfig , tab) {
     var style = cellStyle(feedConfig);
-    //if(isTablet){
-    //    // Create a left panel for tablets only.
-    //    tabris.create("Composite", { left: 0, right: tabletColumnRight, top: 0, bottom: 0 ,background: "white", elevation: 10}).appendTo(tab);
-    //}
-
     var widget = tabris.create("CollectionView", {
         layoutData: {left: 0,  top: 0,  bottom: (-1)*PRELOAD_CELLS*imageHeight },
         elevation: 20,
         items: [],
-        //right: isTablet? tabletColumnRight : 0,
         right: 0,
         itemHeight: imageHeight,
         refreshEnabled: config.pullToRefresh,
@@ -42,54 +36,29 @@ module.exports = function( feedConfig , tab) {
         _tab: tab,
         initializeCell: function(cell){
             var masterContainer = tabris.create('Composite', style.masterContainer).appendTo(cell);
-
-            var elements = [];
-                //containers: [],
-                //images : [],
-                //overlays : [],
-                //titles : []
-            //};
+            var elementsList = [];
             for (var i=0; i<CELLS_PER_ROW; i++){
-                elements.push({});
-                elements[i].container = tabris.create('Composite', style.container).appendTo(masterContainer);
-                elements[i].image = tabris.create('ImageView', style.image).appendTo(elements[i].container);
-                elements[i].overlay = tabris.create('Composite', style.overlay).appendTo(elements[i].container);
-                elements[i].title = tabris.create('TextView',  style.title).appendTo(elements[i].container);
+                elementsList.push({});
+                elementsList[i].container = tabris.create('Composite', style.container).appendTo(masterContainer).on("touchend", function(target) {
+                    var feedItem = target.get("_feedItem");
+                    detailScreen.open(feedItem.title, feedItem);
+                });
+                elementsList[i].image   = tabris.create('ImageView', style.image).appendTo(elementsList[i].container);
+                elementsList[i].overlay = tabris.create('Composite', style.overlay).appendTo(elementsList[i].container);
+                elementsList[i].title   = tabris.create('TextView',  style.title).appendTo(elementsList[i].container);
             }
 
-            //var container = tabris.create('Composite', style.container).appendTo(masterContainer),
-            //    icon      = tabris.create('ImageView', style.image).appendTo(container),
-            //    overlay   = tabris.create('Composite', style.overlay).appendTo(container),
-            //    title     = tabris.create('TextView',  style.title).appendTo(container);
-
             cell.on("change:item", function(widget, feedItems) {
-                updateCellMultipleElements(feedItems, elements);
+                feedItems._elementsList = elementsList;
+                updateCellMultipleElements(feedItems, elementsList);
             });
         }
-    }).on("select", function(target, feedItem) {
-        feedItem.watched = true;
-        updateCellItemElements(feedItem);
-
-        //if(sizing.isTablet){
-        //    if(tab.get('_tabletHtmlContainer')){
-        //        tab.get('_tabletHtmlContainer').get('_itemWebView').dispose();
-        //    }
-        //    else {
-        //        var tabletHtmlContainer = tabris.create("Composite", { left: tabletColumnLeft, right: 0, top: 0, bottom: 0 ,background: "white", elevation: 0}).appendTo(tab);
-        //        tab.set('_tabletHtmlContainer', tabletHtmlContainer);
-        //        // For iOS ?
-        //        // tabris.create("Composite", { left: 0, width: 1, top: 0, bottom: 0 ,background: style.overlay.background , opacity: 0.6}).appendTo(tabletHtmlContainer);
-        //    }
-        //    detailScreen.addItemWebView(tab.get('_tabletHtmlContainer'),feedItem);
-        //}
-        //else {
-            detailScreen.open(feedItem.title, feedItem);
-        //}
-    }).on("scroll", function(widget, scroll) {
+    })
+    .on("scroll", function(widget, scroll) {
         if( widget.get('_loadingNext') || widget.get('_loadedAll') ) { return; }
         if (scroll.deltaY > 0) {
             var remaining = widget.get("items").length - widget.get("lastVisibleIndex");
-            if (remaining < 10) {
+            if (remaining < 8) {
                 loadMoreItems(widget);
             }
 
@@ -111,7 +80,6 @@ function cellStyle(feedConfig){
     return {
         container : { left: "prev()", width: Math.floor(imageWidth / CELLS_PER_ROW), top: 0, bottom: 0 , background: themeStyle.background},
         masterContainer : { left: 0, right: 0, top: 0, bottom: 0 , background: themeStyle.background},
-        //image: { left: 0, right: 0, top: 1, bottom: 1, scaleMode: 'fill' , background: "rgb(220, 220, 220)"},
         image: { left: 0, right: 0, top: 1, bottom: 1, scaleMode: 'fill' , background: "white"},
         overlay: { left: 0, right: 0, height: 46, bottom: 1 ,background: themeStyle.overlayBG, opacity: 0.8},
         title: { maxLines: 2, font: '16px', left: 10, right: 10, bottom: 5, textColor: themeStyle.textColor }
@@ -137,8 +105,6 @@ function refreshItems( widget , forceFetch) {
         widget.set('items', arr );
         widget.set('_loadedPage', 1);
 
-
-        //widget.set('items', items );
         updateWidgetLoading ( widget, false );
 
     }).catch(function(err){
@@ -191,40 +157,47 @@ function updateWidgetLoading(widget,loading){
     });
 }
 
-function updateCellMultipleElements(feedItems, elements){
-    feedItems.forEach(function(feedItem,index){
-        updateCellItemElements(feedItem, elements[index]);
-    });
+function updateCellMultipleElements(feedItems, elementsList){
+    // Dummy for preloading
+    //if(feedItems._dummy){
+    //    //elements.title.set({text:"THIS IS A DUMMY ITEM"});
+    //
+    //    return;
+    //}
+
+    elementsList.forEach(function( elements, index ) {
+        elements.container.set("_feedItem",feedItems[index]);
+        updateCellItemElements(feedItems[index], elements);
+    })
 }
 
 
 function updateCellItemElements(feedItem, elements){
-  var imageUpdate = {opacity: feedItem.watched ? 0.5 : 1};
+  if(!feedItem) {
+      hideElements(elements);
+      return;
+  }
+  elements.container.set({opacity:1});
+  var imageUpdate = {opacity: 1};
   var imageUrl = resizeImageURLByWidth(feedItem.image, imageWidth);
 
   // Image update
-  if(!imageUrl || imageUrl.length === 0 || feedItem._dummy) {
+  if(!imageUrl || imageUrl.length === 0) {
     imageUpdate.opacity = 0;
   }
   else if(  !(elements.image.get('image') && elements.image.get('image').src === imageUrl)){
+      // Image actually changed
       elements.image.set( {image: undefined });
       imageUpdate.image =  {src: imageUrl};
   }
-
   setTimeout(function(){
     elements.image.set( imageUpdate );
   },1);
 
 
-  // Dummy for preloading
-  if(feedItem._dummy){
-      elements.title.set({text:"THIS IS A DUMMY ITEM"});
-      return;
-  }
 
   // Title + Overlay update
   elements.title.set({text: feedItem.title});
-  elements.overlay.set({opacity: feedItem.watched ? 0.5 : 0.8} );
   if(!imageUrl || imageUrl.length === 0) {
     elements.overlay.set({ top: 1, height:undefined });
     elements.title.set({ maxLines: 5});
@@ -234,9 +207,12 @@ function updateCellItemElements(feedItem, elements){
   }
 
     // Loading next.
-    if(feedItem._loadingNext){
-        elements.title.set({text: "Loading more items..."});
-        //TODO: remove the loading animation at the end of feed.
-    }
+    //if(feedItem._loadingNext){
+    //    elements.title.set({text: "Loading more items..."});
+    //    //TODO: remove the loading animation at the end of feed.
+    //}
+}
 
+function hideElements(elements){
+    elements.container.set({opacity:0});
 }
